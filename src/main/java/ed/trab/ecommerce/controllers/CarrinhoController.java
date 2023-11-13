4ceb9a1df40521c8a64ea251394f4105f3e9cf3e
Guilherme Serafim Bonfim <guilhermeserafimbonfim@hotmail.com>
2023-11-12 23:29:41 -0300

    transforma json em pilha

--- src/main/java/ed/trab/ecommerce/controllers/CarrinhoController.java
+++ src/main/java/ed/trab/ecommerce/controllers/CarrinhoController.java
@@ -1,18 +1,14 @@
 package ed.trab.ecommerce.controllers;
 
+import java.util.Map;
 import java.util.Queue;
-import java.util.Stack;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,22 +20,22 @@ import ed.trab.ecommerce.services.CarrinhoService;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class CarrinhoController {
-    
+
     private CarrinhoService carrinhoService;
 
     @Autowired
-    public CarrinhoController(CarrinhoService carrinhoService){
+    public CarrinhoController(CarrinhoService carrinhoService) {
         this.carrinhoService = carrinhoService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public Queue<Produto> checkout(@Param("pilha") Produto[] pilha) throws ResponseStatusException{
-        // try {
+    public Queue<Produto> checkout(@RequestBody() Map<String, String> pilha) throws ResponseStatusException {
+        try {
             return carrinhoService.checkout(pilha);
-        // } catch (Exception e) {
-        //     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Carrinho não contem itens.");
-        // }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Carrinho não contem itens.");
+        }
     }
-    
+
 }
