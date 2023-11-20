03994a00bfdace88c1f4842ac8670c504aeea295
g4bri0s <g.cristianods@gmail.com>
2023-11-20 18:09:59 -0300

    adicionando consulta de cliente por nome

--- src/main/java/ed/trab/ecommerce/repositories/ClienteRepository.java
+++ src/main/java/ed/trab/ecommerce/repositories/ClienteRepository.java
@@ -2,7 +2,10 @@ package ed.trab.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ed.trab.ecommerce.models.Cliente;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+    @Query(value = "SELECT cliente FROM Cliente cliente where LLOWER(cliente.nome) LIKE LOWER(CONCAT('%', :nome, '%'))")
+    Cliente findByNome(String nome);
 }
